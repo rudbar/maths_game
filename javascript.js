5feb05915fa74e2352f9ae565a8af9bfecefa3d0
Jan Kachan <31417194+rudbar@users.noreply.github.com>
2019-09-23 19:43:19 +0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,23 @@
+//if we click on the start/reset button
+	//if we are playing
+		//reload page
+	//if we are not playing 
+		//set score to 0
+		//show countdown box
+		//reduce time by 1sec in loops
+			//timeleft?
+				//yes->continue
+				//no->gameover
+		//change button to reset
+		//generate new Q&A
+
+
+//if we click on answer box
+	// if we are playing
+		//correct?
+			//yes
+				//increase score
+				//show correct box for 1 sec
+				//generate new Q&A
+			//no 
+				//show try again box for 1 sec
