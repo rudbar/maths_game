10336b69a8f40781d538cc3d3f58f346a5d83302
Jan Kachan <31417194+rudbar@users.noreply.github.com>
2019-09-24 05:07:31 +0300

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -21,3 +21,63 @@
 				//generate new Q&A
 			//no 
 				//show try again box for 1 sec
+
+/*if (#startreset is clicked) {
+	if (#question is filled) {
+		tag the whole html and = reload it;
+	}
+	set #scorevalue = 0;
+	set #timeremaining = visible;
+	tag #timeremainingvalue - 1 * n;
+		if (#timeremainingvalue > 0) {
+			continue;
+		}
+		stop;
+	tag #startreset = reset;
+	make new Q&A;
+}
+
+if (#instruction is clicked) {
+	if (#question is filled) {
+		if(#box1 #box2 #box3 #box4 = correct_box) {
+			tag #scorevalue + 1;
+			show correct_box;
+			make new Q&A;
+		}
+			tag #wrong = visible;
+	}
+} */
+
+//================================================================
+
+var playing = false;
+var score;
+
+//if we click on the start/reset button
+document.getElementById("startreset").onclick = function() {
+	//if we are playing
+	if(playing == true){
+
+		location.reload(); //reload page
+	}else{//if we are not playing 
+
+		//change mode to playing
+		playing = true;
+
+		//set score to 0
+		score = 0;
+
+		document.getElementById("scorevalue").innerHTML = score;
+
+		//show countdown box
+
+		document.getElementById("timeremaining").style.display = "block";
+
+		//change button to reset
+
+		document.getElementById("startreset").innerHTML = "Reset Game";
+
+		
+	}
+
+}
